5496b46f8c8c01919fef01c35cd25337b84fc8e8
leilaweicman <leilaweic@hotmail.com>
2025-09-09 13:55:48 +0200

    Change attributes to private

--- src/instruments/Instrument.java
+++ src/instruments/Instrument.java
@@ -1,13 +1,20 @@
 package instruments;
 
 public abstract class Instrument {
-    protected String name;
-    protected double price;
+    private String name;
+    private double price;
 
     public Instrument(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
     public abstract void play();
 }
